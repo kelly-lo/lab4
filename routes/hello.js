f091fc22c2bedc007a9ff0ff190e76b22f37327f
kelly-lo <lo.kelly626@gmail.com>
2021-01-27 23:24:02 -0800

    node.js lab

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,11 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+  var nameToShow = req.params.userName;
+  res.render("hello", {
+    "name": nameToShow
   });
+  // res.render('index', {
+  // 	'name': '',
+  // });
 };
